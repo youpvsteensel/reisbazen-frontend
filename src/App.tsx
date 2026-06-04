b2e80be93457b938d301159dac7aa0f27bbc065a
youpvsteensel <youpvsteensel@gmail.com>
2026-06-04 15:24:20 +0200

    Auto-commit: App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Nav from './components/Nav';
 import HomePage from './pages/HomePage';
+import PatagoniePage from './pages/PatagoniePage';
 import CarreteraAustralPage from './pages/CarreteraAustralPage';
 import ElChaltenPage from './pages/ElChaltenPage';
 import UshuaiaPage from './pages/UshuaiaPage';
@@ -13,10 +14,11 @@ export default function App() {
       <main className="pt-0">
         <Routes>
           <Route path="/" element={<HomePage />} />
-          <Route path="/carretera-austral" element={<CarreteraAustralPage />} />
-          <Route path="/el-chalten" element={<ElChaltenPage />} />
-          <Route path="/ushuaia" element={<UshuaiaPage />} />
-          <Route path="/falklands" element={<FalklandsPage />} />
+          <Route path="/patagonie" element={<PatagoniePage />} />
+          <Route path="/patagonie/carretera-austral" element={<CarreteraAustralPage />} />
+          <Route path="/patagonie/el-chalten" element={<ElChaltenPage />} />
+          <Route path="/patagonie/ushuaia" element={<UshuaiaPage />} />
+          <Route path="/patagonie/falklands" element={<FalklandsPage />} />
         </Routes>
       </main>
     </BrowserRouter>
