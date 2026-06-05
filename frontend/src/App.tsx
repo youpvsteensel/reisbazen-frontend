294e191f93fff55ee9bf308e3ac44096f89f6f2f
youpvsteensel <youpvsteensel@gmail.com>
2026-06-05 16:43:20 +0200

    Auto-commit: App.tsx

---
+++ frontend/src/App.tsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+import Nav from './components/Nav';
+import HomePage from './pages/HomePage';
+import PatagoniePage from './pages/PatagoniePage';
+import CarreteraAustralPage from './pages/CarreteraAustralPage';
+import ElChaltenPage from './pages/ElChaltenPage';
+import UshuaiaPage from './pages/UshuaiaPage';
+import FalklandsPage from './pages/FalklandsPage';
+import PatagonieVolledigheidPage from './pages/PatagonieVolledigheidPage';
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  useEffect(() => { window.scrollTo(0, 0); }, [pathname]);
+  return null;
+}
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <ScrollToTop />
+      <Nav />
+      <main className="pt-0">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/patagonie" element={<PatagoniePage />} />
+          <Route path="/patagonie/volledig" element={<PatagonieVolledigheidPage />} />
+          <Route path="/patagonie/carretera-austral" element={<CarreteraAustralPage />} />
+          <Route path="/patagonie/el-chalten" element={<ElChaltenPage />} />
+          <Route path="/patagonie/ushuaia" element={<UshuaiaPage />} />
+          <Route path="/patagonie/falklands" element={<FalklandsPage />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+}
