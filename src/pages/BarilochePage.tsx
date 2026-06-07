d7c7c9bd1045fffc1e67af6229cedf98cc20df2c
youpvsteensel <youpvsteensel@gmail.com>
2026-06-07 20:06:43 +0200

    Auto-commit: BarilochePage.tsx

---
+++ src/pages/BarilochePage.tsx
@@ -0,0 +1,6 @@
+import BlokPagina from '../components/BlokPagina';
+import { blokken } from '../data/reisData';
+
+export default function BarilochePage() {
+  return <BlokPagina blok={blokken.bariloche} />;
+}
