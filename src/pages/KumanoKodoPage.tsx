98b2c43dd6e8fa4ee6ac23ff60b007f30fc6ff05
youpvsteensel <youpvsteensel@gmail.com>
2026-06-08 17:17:35 +0200

    Auto-commit: KumanoKodoPage.tsx

---
+++ src/pages/KumanoKodoPage.tsx
@@ -0,0 +1,6 @@
+import BlokPagina from '../components/BlokPagina';
+import { blokken } from '../data/reisData';
+
+export default function KumanoKodoPage() {
+  return <BlokPagina blok={blokken.kumano} />;
+}
