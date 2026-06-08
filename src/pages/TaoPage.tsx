dc6c9325affa9e3929b26b9bcc63256e22247061
youpvsteensel <youpvsteensel@gmail.com>
2026-06-08 17:17:08 +0200

    Auto-commit: TaoPage.tsx

---
+++ src/pages/TaoPage.tsx
@@ -0,0 +1,6 @@
+import BlokPagina from '../components/BlokPagina';
+import { blokken } from '../data/reisData';
+
+export default function TaoPage() {
+  return <BlokPagina blok={blokken.tao} />;
+}
