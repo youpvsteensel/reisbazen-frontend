e83764f0644c360c166777464a276866428cdb4d
youpvsteensel <youpvsteensel@gmail.com>
2026-06-08 17:16:50 +0200

    Auto-commit: PortBartonPage.tsx

---
+++ src/pages/PortBartonPage.tsx
@@ -0,0 +1,6 @@
+import BlokPagina from '../components/BlokPagina';
+import { blokken } from '../data/reisData';
+
+export default function PortBartonPage() {
+  return <BlokPagina blok={blokken.portBarton} />;
+}
