f81f6932336b29bc542c8213299f319b54ecaaf3
youpvsteensel <youpvsteensel@gmail.com>
2026-06-08 17:17:21 +0200

    Auto-commit: SmangusPage.tsx

---
+++ src/pages/SmangusPage.tsx
@@ -0,0 +1,6 @@
+import BlokPagina from '../components/BlokPagina';
+import { blokken } from '../data/reisData';
+
+export default function SmangusPage() {
+  return <BlokPagina blok={blokken.smangus} />;
+}
