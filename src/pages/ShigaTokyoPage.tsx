95893bfbe5180d7e2f459e1af634da5fddff84ab
youpvsteensel <youpvsteensel@gmail.com>
2026-06-08 17:17:51 +0200

    Auto-commit: ShigaTokyoPage.tsx

---
+++ src/pages/ShigaTokyoPage.tsx
@@ -0,0 +1,6 @@
+import BlokPagina from '../components/BlokPagina';
+import { blokken } from '../data/reisData';
+
+export default function ShigaTokyoPage() {
+  return <BlokPagina blok={blokken.shiga} />;
+}
