be7eb8b488e265d6715b943a3ba1f34f49682687
youpvsteensel <youpvsteensel@gmail.com>
2026-06-08 17:16:59 +0200

    Auto-commit: ElNidoPage.tsx

---
+++ src/pages/ElNidoPage.tsx
@@ -0,0 +1,6 @@
+import BlokPagina from '../components/BlokPagina';
+import { blokken } from '../data/reisData';
+
+export default function ElNidoPage() {
+  return <BlokPagina blok={blokken.elNido} />;
+}
