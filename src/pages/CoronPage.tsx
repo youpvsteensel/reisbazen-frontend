af93309e41b4b26738f9a5c4c90d177a067e8c71
youpvsteensel <youpvsteensel@gmail.com>
2026-06-08 17:17:14 +0200

    Auto-commit: CoronPage.tsx

---
+++ src/pages/CoronPage.tsx
@@ -0,0 +1,6 @@
+import BlokPagina from '../components/BlokPagina';
+import { blokken } from '../data/reisData';
+
+export default function CoronPage() {
+  return <BlokPagina blok={blokken.coron} />;
+}
