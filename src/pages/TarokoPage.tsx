03778ed1b4cb3308717a94c3415693a7ab2f6fa0
youpvsteensel <youpvsteensel@gmail.com>
2026-06-08 17:17:27 +0200

    Auto-commit: TarokoPage.tsx

---
+++ src/pages/TarokoPage.tsx
@@ -0,0 +1,6 @@
+import BlokPagina from '../components/BlokPagina';
+import { blokken } from '../data/reisData';
+
+export default function TarokoPage() {
+  return <BlokPagina blok={blokken.taroko} />;
+}
