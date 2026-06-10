c0359e5f9792fe525bd18674ba52be66fbf7cd98
youpvsteensel <youpvsteensel@gmail.com>
2026-06-10 11:25:41 +0200

    Auto-commit: TasmaniePage.tsx

---
+++ src/pages/TasmaniePage.tsx
@@ -0,0 +1,6 @@
+import BlokPagina from '../components/BlokPagina';
+import { blokken } from '../data/reisData';
+
+export default function TasmaniePage() {
+  return <BlokPagina blok={blokken.tasmanie} />;
+}
