b102154e17b77fa7b58cd469b7f0cc30b73f73f5
youpvsteensel <youpvsteensel@gmail.com>
2026-06-10 11:25:48 +0200

    Auto-commit: WestAustraliePage.tsx

---
+++ src/pages/WestAustraliePage.tsx
@@ -0,0 +1,6 @@
+import BlokPagina from '../components/BlokPagina';
+import { blokken } from '../data/reisData';
+
+export default function WestAustraliePage() {
+  return <BlokPagina blok={blokken.westAustralie} />;
+}
